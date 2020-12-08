827e7db25e204aa324dddfe6e460be66957afa85
Max <maxblanck@outlook.fr>
2020-12-08 16:42:26 +0100

    php Partie 4  Exercice 3

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+function returnstring($firstString = 'Hello You', $secondString = 'Little Bastard')
+{
+    return $firstString . ' ' . $secondString;
+}
+?>
+
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=, initial-scale=1.0" />
+    <title>Exercice 2 Partie 4</title>
+</head>
+
+<body>
+    <h1>Exercice 2 Partie 4</h1>
+    <p><?php
+        echo returnstring();
+    ?></p>
+</body>
+
+</html>
